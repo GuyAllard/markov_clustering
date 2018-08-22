9efecdfa582652f91ebd164ad3c6d84f239b4b13
Guy Allard <guyallard01@gmail.com>
2018-08-22 13:31:27 +0200

    added Jona Harris to author list (for contributions in pull request #3)

--- markov_clustering/__init__.py
+++ markov_clustering/__init__.py
@@ -9,6 +9,6 @@ __version_info__ = ("0", "0", "3", "dev")
 __date__ = "28 Sep 2017"
 
 __version__ = ".".join(__version_info__)
-__author__ = "Guy Allard"
+__author__ = "Guy Allard, Jona Harris"
 __contact__ = "guyallard01@gmail.com"
 __homepage__ = "https://github.com/guyallard/markov_clustering.git"
